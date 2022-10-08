2e8544282b0d3d4e454e5b996e1bcf63a93e91c5
NamdevTukaram007 <115356991+NamdevTukaram007@users.noreply.github.com>
2022-10-08 19:04:54 +0530

    sendmail() method of the SMTP object
    There are cases where the emails are sent using the Gmail SMTP server. In this case, we can pass Gmail as the SMTP server instead of using the localhost with the port 587.

--- Test.py
+++ Test.py
@@ -69,7 +69,19 @@ while True:
             cv2.imshow('Emotion Detector',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+ender_mail = 'sender@fromdomain.com'    
+receivers_mail = ['reciever@todomain.com']    
+message = """From: From Person %s  
+To: To Person %s  
+Subject: Sending SMTP e-mail   
+This is a test e-mail message.  
+"""%(sender_mail,receivers_mail)    
+try:    
+   smtpObj = smtplib.SMTP('localhost')    
+   smtpObj.sendmail(sender_mail, receivers_mail, message)    
+   print("Successfully sent email")    
+except Exception:    
+   print("Error: unable to send email")    
 #Exit
 cap.release()
 cv2.destroyAllWindows()
