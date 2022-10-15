1a343bcaaf103357f6d565d8bfe72804adfb3d97
Minu Gavand <91831080+minugavand@users.noreply.github.com>
2022-10-16 00:25:01 +0530

    Update Classification_mobile_net.py

--- Classification_mobile_net.py
+++ Classification_mobile_net.py
@@ -89,6 +89,24 @@ def fibonacci(n):
 	else:	
 		FibArray.append(fibonacci(n - 1) + fibonacci(n - 2))
 		return FibArray[n]
+# Python3 code to demonstrate working of
+# Alternate K Length characters
+# Using list comprehension + join()
+
+# initializing string
+test_str = 'geeksgeeksisbestforgeeks'
+
+# printing original string
+print("The original string is : " + str(test_str))
+
+# initializing K
+K = 4
+
+# slicing K using slicing, join for converting back to string
+res = ''.join([test_str[idx : idx + K] for idx in range(0, len(test_str), K * 2)])
+
+# printing result
+print("Transformed String : " + str(res))
 
 # Driver Program
 print(fibonacci(9))
